f24930afed12f138097d2d5f29e4ec182680ee45
Vlados <Mxbox360@yandex.ru>
2018-07-06 14:04:59 +0300

    new file:   .idea/compiler.xml 	new file:   .idea/libraries/Maven__junit_junit_4_12.xml 	new file:   .idea/libraries/Maven__org_hamcrest_hamcrest_core_1_3.xml 	new file:   .idea/misc.xml 	new file:   .idea/modules.xml 	new file:   .idea/workspace.xml 	new file:   Testss.iml 	new file:   pom.xml 	new file:   src/main/java/Main.java 	new file:   src/test/java/Testprog.java 	new file:   target/classes/Main.class 	new file:   target/test-classes/Testprog.class

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+   public double sum(double a, double b){
+       return a+b;
+   }
+   public double sub(double a, double b){
+      return a-b;
+   }
+   public double mul(double a, double b){
+      return a*b;
+   }
+   public double div(double a, double b){
+      return a/b;
+   }
+}
---
+++ src/test/java/Testprog.java
@@ -0,0 +1,28 @@
+import org.junit.Test;
+import static junit.framework.TestCase.*;
+public class Testprog{
+    @Test
+    public void sumTest() {
+        Main tt = new Main();
+        double t = tt.sum(10.0, 5.0);
+        assertEquals(15.0, t);
+    }
+    @Test
+    public void subTest() {
+        Main tt = new Main();
+        double t = tt.sub(10.0, 5.0);
+        assertEquals(5.0, t);
+    }
+    @Test
+    public void mulTest() {
+        Main tt = new Main();
+        double t = tt.mul(10.0, 5.0);
+        assertEquals(50.0, t);
+    }
+    @Test
+    public void divTest() {
+        Main tt = new Main();
+        double t = tt.div(10.0, 5.0);
+        assertEquals(2.0, t);
+    }
+}
